55e9294ced05ec3b544004a2c305faaaf3be249c
dewifitri <162424080+dewifitri@users.noreply.github.com>
2024-03-06 15:35:45 +0700

    Create amik.php

---
+++ amik.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Kampus</title>
+  </head>
+  <body>
+    <?php 
+      echo "Amik Mahaputra Riau";
+      echo "Nim : 22110045";
+      echo "Nama : Dewi fitriyani";
+      echo "Kelas : 22Mi-A";
+      echo "Alamat : JL. Soebrantas";
+    ?>
+  </body>
+</html>
